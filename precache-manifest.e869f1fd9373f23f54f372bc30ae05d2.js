0d26db8985f17be250f2b139c65cff88e0c0a59c
naufal-dean <naufal.dean@gmail.com>
2020-04-06 13:07:05 +0700

    Updates

---
+++ precache-manifest.e869f1fd9373f23f54f372bc30ae05d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cad4a1cb20696f7dc261cdc718ec2c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "19029f53a471cc789a6b",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d85990def6b0ded30cc1",
+    "url": "/portfolio/static/js/2.b6cc0dcc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.b6cc0dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19029f53a471cc789a6b",
+    "url": "/portfolio/static/js/main.215e6bb4.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
