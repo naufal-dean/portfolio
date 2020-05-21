dc056390591451f341864ecdf6ede23a6bcfab92
naufal-dean <naufal.dean@gmail.com>
2020-05-22 01:09:36 +0700

    Switch popup to react Rodal, little bug fix

--- src/component/ProjectPopup/ProjectPopup.js
+++ src/component/ProjectPopup/ProjectPopup.js
@@ -13,30 +13,27 @@ const ProjectPopup = props => {
       closeMaskOnClick
       customStyles={{ padding: `0`, height: `90vh`, width: `100vh` }}
     >
-        <div className="project-popup">
-          <div className="popup-img">
-            <img
-              src={require(`../../image/proj/${props.image}`)}
-              alt="Project"
-            />
-          </div>
-          <div className="popup-title">
-            <h3>{props.title}</h3>
-          </div>
-          <div className="popup-text">
-            <p>{props.text}</p>
-          </div>
-          <div className="popup-action">
-            {props.github && (
-              <button
-                className="ghost-btn"
-                onClick={() => window.open(`${props.github}`)}
-              >
-                <i class="fab fa-github"></i>&nbsp;View Code
-              </button>
-            )}
-          </div>
+      <div className="project-popup">
+        <div className="popup-img">
+          <img src={require(`../../image/proj/${props.image}`)} alt="Project" />
         </div>
+        <div className="popup-title">
+          <h3>{props.title}</h3>
+        </div>
+        <div className="popup-text">
+          <p>{props.text}</p>
+        </div>
+        <div className="popup-action">
+          {props.github && (
+            <button
+              className="ghost-btn"
+              onClick={() => window.open(`${props.github}`)}
+            >
+              <i class="fab fa-github"></i>&nbsp;View Code
+            </button>
+          )}
+        </div>
+      </div>
     </Rodal>
   );
 };
