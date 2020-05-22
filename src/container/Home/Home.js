5946df75408764a5f196ece67ecc7a8465ee9d3d
naufal-dean <naufal.dean@gmail.com>
2020-05-22 19:08:17 +0700

    Add about me button animation, add webkit compatibility

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -11,8 +11,11 @@ const Home = props => {
         </h2>
         <h3>I'm a student at Informatics Engineering ITB</h3>
         <h3>And I love creating something awesome</h3>
-        <button onClick={props.gotoAbout}>About me</button>
+        {/* <button onClick={props.gotoAbout}>About me</button> */}
       </span>
+      <div className="about-me" onClick={props.gotoAbout}>
+        <span></span><p>About me</p>
+      </div>
     </div>
   );
 };
